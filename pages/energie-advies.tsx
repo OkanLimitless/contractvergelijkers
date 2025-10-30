0f9e91bedd66f10e046b417cbcc273f39eb140d3
OkanLimitless <okangalatasaray001@gmail.com>
2025-10-30 09:48:31 +0100

    Update Google tag tracking for energie-advies page

--- pages/energie-advies.tsx
+++ pages/energie-advies.tsx
@@ -137,14 +137,14 @@ export default function EnergieAdvies() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
 
-        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17683968777" />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17687373741"></script>
         <script
           dangerouslySetInnerHTML={{
             __html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', 'AW-17683968777');
+              gtag('config', 'AW-17687373741');
               function gtag_report_conversion(url) {
                 var callback = function () {
                   if (typeof(url) != 'undefined') {
@@ -152,7 +152,7 @@ export default function EnergieAdvies() {
                   }
                 };
                 gtag('event', 'conversion', {
-                  'send_to': 'AW-17683968777/l1NqCIzdj7UbEInmr_BB',
+                  'send_to': 'AW-17687373741/UuVBCKaBwLYbEK3P__FB',
                   'value': 1.0,
                   'currency': 'USD',
                   'event_callback': callback
